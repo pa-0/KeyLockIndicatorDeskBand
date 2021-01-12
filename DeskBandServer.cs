f73488efc62a3f244419df50a8ea11aac7b6f948
kiennt <ntkien45@gmail.com>
2021-01-12 12:17:36 +0700

    m

---
+++ DeskBandServer.cs
@@ -0,0 +1,35 @@
+﻿using SharpShell.Attributes;
+using SharpShell.SharpDeskBand;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeyLockIndicatorDeskBand
+{
+    [ComVisible(true)]
+    [DisplayName("Web Search")]
+    public class DeskBandServer : SharpDeskBand
+    {
+        protected override UserControl CreateDeskBand()
+        {
+            return new UserControl1();
+        }
+
+        protected override BandOptions GetBandOptions()
+        {
+            return new BandOptions
+            {
+                HasVariableHeight = false,
+                IsSunken = false,
+                ShowTitle = false,
+                Title = "",
+                UseBackgroundColour = false,
+                AlwaysShowGripper = true
+            };
+        }
+    }
+}
