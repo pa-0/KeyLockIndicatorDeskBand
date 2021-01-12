4cb4a4e85edcae55b9ff84a9ca17547336f4844c
kiennt <ntkien45@gmail.com>
2021-01-12 10:04:43 +0700

    commit before Test

--- UserControl1.Designer.cs
+++ UserControl1.Designer.cs
@@ -28,11 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.components = new System.ComponentModel.Container();
+            this.lbNum = new System.Windows.Forms.Label();
+            this.lbCaps = new System.Windows.Forms.Label();
+            this.lbScroll = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // lbNum
+            // 
+            this.lbNum.BackColor = System.Drawing.Color.Blue;
+            this.lbNum.Location = new System.Drawing.Point(3, 0);
+            this.lbNum.Name = "lbNum";
+            this.lbNum.Size = new System.Drawing.Size(100, 23);
+            this.lbNum.TabIndex = 0;
+            this.lbNum.Text = "label1";
+            this.toolTip1.SetToolTip(this.lbNum, "Num Lock");
+            this.lbNum.Click += new System.EventHandler(this.lbNum_Click);
+            // 
+            // lbCaps
+            // 
+            this.lbCaps.BackColor = System.Drawing.Color.Red;
+            this.lbCaps.Location = new System.Drawing.Point(109, 0);
+            this.lbCaps.Name = "lbCaps";
+            this.lbCaps.Size = new System.Drawing.Size(100, 23);
+            this.lbCaps.TabIndex = 1;
+            this.lbCaps.Text = "label2";
+            this.toolTip1.SetToolTip(this.lbCaps, "Caps Lock");
+            this.lbCaps.Click += new System.EventHandler(this.lbCaps_Click);
+            // 
+            // lbScroll
+            // 
+            this.lbScroll.BackColor = System.Drawing.Color.Lime;
+            this.lbScroll.Location = new System.Drawing.Point(215, 0);
+            this.lbScroll.Name = "lbScroll";
+            this.lbScroll.Size = new System.Drawing.Size(100, 23);
+            this.lbScroll.TabIndex = 1;
+            this.lbScroll.Text = "label3";
+            this.toolTip1.SetToolTip(this.lbScroll, "Srcoll Lock");
+            this.lbScroll.Click += new System.EventHandler(this.lbScroll_Click);
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbScroll);
+            this.Controls.Add(this.lbCaps);
+            this.Controls.Add(this.lbNum);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(318, 29);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbNum;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label lbCaps;
+        private System.Windows.Forms.Label lbScroll;
     }
 }
--- UserControl1.cs
+++ UserControl1.cs
@@ -12,9 +12,64 @@ namespace KeyLockIndicatorDeskBand
 {
     public partial class UserControl1: UserControl
     {
+        UserActivityHook userActivityHook = new UserActivityHook();
         public UserControl1()
         {
             InitializeComponent();
+            userActivityHook.KeyDown += new KeyEventHandler(userActivityHook_KeyDown);
         }
+
+        private void userActivityHook_KeyDown(object sender, KeyEventArgs e)
+        {
+			if (e.KeyCode == Keys.NumLock)
+			{
+				if (!Control.IsKeyLocked(Keys.NumLock))
+				{
+					lbNum.Font = new Font(lbNum.Font, FontStyle.Bold);
+				}
+				else
+				{
+					lbNum.Font = new Font(lbNum.Font, FontStyle.Regular);
+				}
+			}
+			if (e.KeyCode == Keys.Capital)
+			{
+				if (!Control.IsKeyLocked(Keys.Capital))
+				{
+					lbCaps.Font = new Font(lbCaps.Font, FontStyle.Bold);
+				}
+				else
+				{
+					lbCaps.Font = new Font(lbCaps.Font, FontStyle.Regular);
+				}
+			}
+			if (e.KeyCode == Keys.Scroll)
+			{
+				if (!Control.IsKeyLocked(Keys.Scroll))
+				{
+					lbScroll.Font = new Font(lbScroll.Font, FontStyle.Bold);
+				}
+				else
+				{
+					lbScroll.Font = new Font(lbScroll.Font, FontStyle.Regular);
+				}
+			}
+		}
+
+        private void lbNum_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbCaps_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbScroll_Click(object sender, EventArgs e)
+        {
+
+        }
+        
     }
 }
