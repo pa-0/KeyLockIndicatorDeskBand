bdb42ffd2923972fcff056dae0c862cb0592ee29
kien <kien@kien>
2021-01-16 02:44:07 +0700

    fix bug
    x

--- KeyLockIndicatorDeskBand/Installer1.cs
+++ KeyLockIndicatorDeskBand/Installer1.cs
@@ -14,16 +14,19 @@ namespace KeyLockIndicatorDeskBand
     [RunInstaller(true)]
     public partial class Installer1 : System.Configuration.Install.Installer
     {
-        private const string PARAM = "targetdir";
+        private const string PARAM = "TargetDir";
         private const string FILE_NAME = "KeyLockIndicatorDeskBand.dll";
         private readonly string PROGRAM_NAME = GetFrameworkDirectory() + "regasm.exe";
         private const string UNREGISTER = "/unregister ";
         private const string CODEBASE = "/codebase ";
-        private const string BACKSLASH = "\"";
+        private const string DOUBLE_QUOTE = "\"";
         private const string KeydotNetFolder = @"Software\Microsoft\.NetFramework";
         private const string ROOT_KEY = "InstallRoot";
         private const string Format = @"v{0}.{1}.{2}\";
-        
+        private const string CMD = "cmd.exe";
+        private const string PARAM_OUT = "/k ";//K or c
+        private const string SPACE = " ";
+
         public Installer1()
         {
             InitializeComponent();
@@ -31,54 +34,80 @@ namespace KeyLockIndicatorDeskBand
 
         private void Installer1_AfterInstall(object sender, InstallEventArgs e)
         {
-            string path = this.Context.Parameters[PARAM];
-            string filepath = BACKSLASH + path + FILE_NAME + BACKSLASH;
-            string command = PROGRAM_NAME;
-            string args = CODEBASE + filepath;
-            MessageBox.Show("AI");
-            var process = Process.Start(command, args);
-            string info = process.StandardOutput.ReadToEnd();
-            File.WriteAllText(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\AI.txt", info);
-            process.Close();
+            //string path = this.Context.Parameters[PARAM];
+            //if (path.EndsWith(Path.DirectorySeparatorChar.ToString()+ Path.DirectorySeparatorChar))
+            //    path = path.Replace(Path.DirectorySeparatorChar.ToString() + Path.DirectorySeparatorChar, Path.DirectorySeparatorChar.ToString());
+            ////MessageBox.Show(path);
+            //string filepath = DOUBLE_QUOTE + path + FILE_NAME + DOUBLE_QUOTE;
+            //string command = PROGRAM_NAME;
+            //string args = CODEBASE + filepath;
+            ////MessageBox.Show(command);
+            ////MessageBox.Show(args);
+            //ProcessStartInfo proc = new ProcessStartInfo();
+            //proc.FileName = CMD;
+            //proc.Arguments = PARAM_OUT + command + SPACE + args;
+            //Process.Start(proc);
+            ////var process = Process.Start(command, args);
+            ////string info = process.StandardOutput.ReadToEnd();
+            ////File.WriteAllText(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\AI.txt", info);
+            ////process.Close();
         }
 
         private void Installer1_AfterUninstall(object sender, InstallEventArgs e)
         {
             string path = this.Context.Parameters[PARAM];
-            string filepath = BACKSLASH + path + FILE_NAME + BACKSLASH;
+            if (path.EndsWith(Path.DirectorySeparatorChar.ToString() + Path.DirectorySeparatorChar))
+                path = path.Replace(Path.DirectorySeparatorChar.ToString() + Path.DirectorySeparatorChar, Path.DirectorySeparatorChar.ToString());
+            string filepath = DOUBLE_QUOTE + path + FILE_NAME + DOUBLE_QUOTE;
             string command = PROGRAM_NAME;
             string args = UNREGISTER + filepath;
-            MessageBox.Show("AU");
-            var process = Process.Start(command, args);
-            string info = process.StandardOutput.ReadToEnd();
-            File.WriteAllText(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\AU.txt", info);
-            process.Close();
+            //MessageBox.Show("AU");
+            ProcessStartInfo proc = new ProcessStartInfo();
+            proc.FileName = CMD;
+            proc.Arguments = PARAM_OUT + command + SPACE + args;
+            Process.Start(proc);
+            //string info = process.StandardOutput.ReadToEnd();
+            //File.WriteAllText(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\AU.txt", info);
+            //process.Close();
         }
 
         private void Installer1_Committed(object sender, InstallEventArgs e)
         {
             string path = this.Context.Parameters[PARAM];
-            string filepath = BACKSLASH + path + FILE_NAME + BACKSLASH;
+            if (path.EndsWith(Path.DirectorySeparatorChar.ToString() + Path.DirectorySeparatorChar))
+                path = path.Replace(Path.DirectorySeparatorChar.ToString() + Path.DirectorySeparatorChar, Path.DirectorySeparatorChar.ToString());
+            //MessageBox.Show(path);
+            string filepath = DOUBLE_QUOTE + path + FILE_NAME + DOUBLE_QUOTE;
             string command = PROGRAM_NAME;
             string args = CODEBASE + filepath;
-            MessageBox.Show("C");
-            var process = Process.Start(command, args);
-            string info = process.StandardOutput.ReadToEnd();
-            File.WriteAllText(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\C.txt", info);
-            process.Close();
+            //MessageBox.Show(command);
+            //MessageBox.Show(args);
+            ProcessStartInfo proc = new ProcessStartInfo();
+            proc.FileName = CMD;
+            proc.Arguments = PARAM_OUT + command + SPACE + args;
+            Process.Start(proc);
+            //var process = Process.Start(command, args);
+            //string info = process.StandardOutput.ReadToEnd();
+            //File.WriteAllText(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\AI.txt", info);
+            //process.Close();
         }
 
         private void Installer1_AfterRollback(object sender, InstallEventArgs e)
         {
-            string path = this.Context.Parameters[PARAM];
-            string filepath = BACKSLASH + path + FILE_NAME + BACKSLASH;
-            string command = PROGRAM_NAME;
-            string args = UNREGISTER + filepath;
-            MessageBox.Show("AR");
-            var process = Process.Start(command, args);
-            string info = process.StandardOutput.ReadToEnd();
-            File.WriteAllText(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\AR.txt", info);
-            process.Close();
+            //string path = this.Context.Parameters[PARAM];
+            //if (path.EndsWith(Path.DirectorySeparatorChar.ToString() + Path.DirectorySeparatorChar))
+            //    path = path.Replace(Path.DirectorySeparatorChar.ToString() + Path.DirectorySeparatorChar, Path.DirectorySeparatorChar.ToString());
+            //string filepath = DOUBLE_QUOTE + path + FILE_NAME + DOUBLE_QUOTE;
+            //string command = PROGRAM_NAME;
+            //string args = UNREGISTER + filepath;
+            ////MessageBox.Show("AR");
+            //ProcessStartInfo proc = new ProcessStartInfo();
+            //proc.FileName = CMD;
+            //proc.Arguments = PARAM_OUT + command + SPACE + args;
+            //Process.Start(proc);
+            ////string info = process.StandardOutput.ReadToEnd();
+            ////File.WriteAllText(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\AR.txt", info);
+            ////process.Close();
         }
 
         public static string GetFrameworkDirectory()
